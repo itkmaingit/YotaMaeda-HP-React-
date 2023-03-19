02006f31164ae84abe958adbcacae651b6fbbf36
itkmaingit <itkmain.git@gmail.com>
2023-03-19 06:07:46 +0000

    react-useとreact-responsiveをinstall

--- next.config.js
+++ next.config.js
@@ -10,19 +10,6 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      // クライアント側でのみEmotionを有効にする
-      config.resolve.alias["@emotion/core"] = "@emotion/react";
-      config.resolve.alias["emotion-theming"] = "@emotion/react";
-    }
-    return config;
-  },
-  babel(config) {
-    // EmotionのJSXプラグインをBabelに追加する
-    config.plugins.push(["@emotion/babel-plugin"]);
-    return config;
-  },
 };
 
 module.exports = nextConfig;
